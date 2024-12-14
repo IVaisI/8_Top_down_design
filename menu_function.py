3b77e3420f74a499f22ef3ae2c347151d12f3437
IVaisI <ponomaryovnikita2000@gmail.com>
2024-12-14 18:04:51 +0700

    menu + stubs

---
+++ menu_function.py
@@ -0,0 +1,19 @@
+from pyexpat.errors import messages
+from new_functions import input_text, random_text, is_int
+from collections import Counter
+import re
+def f1():
+    global message
+    print("Выберите тип ввода:\n1. Ввод текста с клавиатуры\n2. Случайная генерация")
+
+    return True
+
+def f2():
+
+    print("Алгоритм выполнен\n")
+    return 1
+
+def f3(word_count):
+    print("Количество слов в тексте: 7")
+def print_text():
+    print("от топота копыт пыль по полю летит")
---
+++ new_functions.py
@@ -0,0 +1,12 @@
+import random
+def is_int(choice):
+    return True
+
+
+def input_text():
+    text = "от топота копыт пыль по полю летит "
+    return text
+
+def random_text():
+    letters = "от топота копыт пыль по полю летит"
+#commit
