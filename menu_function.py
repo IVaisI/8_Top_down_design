31211258ee1f2070c4a28b119e12d0d398c0ed9a
IVaisI <ponomaryovnikita2000@gmail.com>
2024-12-14 18:11:44 +0700

    correction

--- menu_function.py
+++ menu_function.py
@@ -5,15 +5,14 @@ import re
 def f1():
     global message
     print("Выберите тип ввода:\n1. Ввод текста с клавиатуры\n2. Случайная генерация")
+    message = input_text()
 
     return True
 
 def f2():
-
     print("Алгоритм выполнен\n")
-    return 1
 
 def f3(word_count):
-    print("Количество слов в тексте: 7")
+    print(f"Количество слов в тексте: 7")
 def print_text():
     print("от топота копыт пыль по полю летит")
